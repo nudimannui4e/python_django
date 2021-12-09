b9f913ea16b90ef79580acd44ee57022bafe5a3c
nudimannui4e <nudimannui4e@gmail.com>
2021-12-09 03:51:44 +0300

    Jinja in html

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about-us', views.about)
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about')
 ]
