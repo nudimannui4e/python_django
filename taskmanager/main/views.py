66de32e4e85331639aa7722ad7dffa52322d09d3
nudimannui4e <nudimannui4e@gmail.com>
2021-12-10 05:16:19 +0300

    tasks

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
+    tasks = Task.objects.all()
     return render(request, 'main/index.html')
 
 
 def about(request):
     return render(request, 'main/about.html')
+
